7938fa41a47f94559f80ed921cc7fed9fff3ceaa
Ivana Miskovic <37186143+ivanamiskovic@users.noreply.github.com>
2021-06-26 12:20:41 +0200

    update

--- Web2 Backend/Web2 Backend/Repository/SwitchingPlanRepository.cs
+++ Web2 Backend/Web2 Backend/Repository/SwitchingPlanRepository.cs
@@ -10,5 +10,24 @@ namespace Web2_Backend.Repository
     public class SwitchingPlanRepository : Repository<SwitchingPlan>, ISwitchingPlanRepository
     {
         public SwitchingPlanRepository(Web2Context context) : base(context) { }
+
+        public override IEnumerable<SwitchingPlan> GetAll()
+        {
+            return Web2Context.SwitchingPlan.Where(x => x.Deleted == false).ToList();
+        }
+
+        public override PageResponse<SwitchingPlan> GetAll(int page, int perPage, string search)
+        {
+            string term = search.ToLower();
+
+            var query = Web2Context.SwitchingPlan.Where(x => x.Street.ToLower().Contains(term)
+            || x.Notes.ToLower().Contains(term) || x.Company.ToLower().Contains(term) || x.Number.ToLower().Contains(term) || x.Point.ToLower().Contains(term) || x.Team.ToLower().Contains(term));
+
+            return new PageResponse<SwitchingPlan>(query.Skip(page * perPage).Take(perPage).ToList(), query.Count());
+        }
+
+
+
+
     }
 }
